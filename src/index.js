c19952bd33ec24948a68c1264ed2ebf03d16e236
SNx42 <seannowotny@live.de>
2019-12-24 12:44:24 +1000

    Bind context to prisma

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
+const { prisma } = require('./generated/prisma-client');
 const { GraphQLServer } = require('graphql-yoga');
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length;
 const resolvers = {
   Query: {
-    info: () => 'This is the API of a Hackernews Clone',
-    feed: () => links
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (root, args, context, info) =>
+    {
+      return context.prisma.links();
+    },
   },
   Mutation: {
-    post: (parent, args) => 
-      {
-        const link = {
-          id: `link-${idCount++}`,
-          description: args.description,
-          url: args.url
-        };
-        links.push(link);
-        return link;
-      }
-  }
-};
+    post: (root, args, context) =>
+    {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description,
+      });
+    },
+  },
+}
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
